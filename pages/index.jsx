2602e982b3f6dc264067943dc342b9aa4c4a5e66
Bruno Mariano <bsignx@gmail.com>
2022-06-15 08:30:37 -0300

    feat: add banner to home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,17 @@
-const Home = () => <h1>aaaa</h1>;
+import { Banner } from '../components';
+
+const Home = () => (
+  <div className="flex justify-center sm:px-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner
+        name={
+          (<>Discover, collect, and sell <br /> extraordinary NFTs</>)
+        }
+        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+        parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+      />
+    </div>
+  </div>
+);
 
 export default Home;
