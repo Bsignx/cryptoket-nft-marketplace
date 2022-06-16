c2c7e4822f226490abf97f4c966e75efa67901a7
Bruno Mariano <bsignx@gmail.com>
2022-06-16 20:45:32 -0300

    feat: create hot bids  section

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import images from '../assets';
 
 import { makeid } from '../utils/makeId';
 
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 
 const Home = () => {
   const [hideButtons, setHideButtons] = useState(false);
@@ -114,6 +114,31 @@ const Home = () => {
                 </>
               )}
             </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">
+              Hot Bids
+            </h1>
+
+            <div>SearchBar</div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeid(3)}...${makeid(4)}`,
+                  owner: `0x${makeid(3)}...${makeid(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
